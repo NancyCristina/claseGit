f12051487d21699ed14737f68055e9483d73ba69
nancy miguel <nancycristina.miguelaguirre@gmail.com>
2022-08-15 15:28:44 -0600

    Todo en orden

--- docker_fastapi/app/main.py
+++ docker_fastapi/app/main.py
@@ -21,25 +21,13 @@ def get_db():
 async def root():
     return {"message": "Hello World this is my new API!"}
 
+    @app.get("/mi_nombre")
+async def root():
+    return {"Nancy Cristina Miguel Aguirre"}
+
 
 @app.get("/myname/{name}")
 async def myName(name: str):
     return {"message": f"Hello {name} this is my new API!"}
 
 
-@app.post("/users/create", response_model=schemas.UserCreate)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_id(db, user.id)
-    if db_user:
-        raise HTTPException(status_code=400, detail="User already registered")
-    return crud.create_user(db=db, user=user)
-
-
-@app.post("/users/{user_id}/", response_model=schemas.UserBase)
-def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
-    return crud.get_user(db=db, user_id=user_id)
-
-
-@app.post("/users/", response_model=List[schemas.UserData])
-async def get_users(db: Session = Depends(get_db)):
-    return crud.get_all_users(db=db)
